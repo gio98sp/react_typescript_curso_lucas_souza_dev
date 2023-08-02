3f383644c643871cc20259f98ff1ba7c3b452a9d
Giovane Santos <giovane98sp@gmail.com>
2023-08-02 10:09:25 -0300

    feat: login page and navigate for dashboard

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Dashboard } from "../pages";
+import { Dashboard, Login } from "../pages";
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/first-page" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="/first-page" />} />
       </Routes>
